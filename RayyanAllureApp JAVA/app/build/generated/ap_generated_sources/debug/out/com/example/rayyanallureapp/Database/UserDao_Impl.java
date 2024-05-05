b29c4352c27c72002eda1a2f81c991ca8061b655
Rayyan Shabbir <rayanshabir1@gmail.com>
2024-05-05 12:29:41 +0500

    Add files via upload

---
+++ RayyanAllureApp JAVA/app/build/generated/ap_generated_sources/debug/out/com/example/rayyanallureapp/Database/UserDao_Impl.java
@@ -0,0 +1,297 @@
+package com.example.rayyanallureapp.Database;
+
+import android.database.Cursor;
+import androidx.lifecycle.LiveData;
+import androidx.room.EntityDeletionOrUpdateAdapter;
+import androidx.room.EntityInsertionAdapter;
+import androidx.room.RoomDatabase;
+import androidx.room.RoomSQLiteQuery;
+import androidx.room.SharedSQLiteStatement;
+import androidx.room.util.CursorUtil;
+import androidx.room.util.DBUtil;
+import androidx.sqlite.db.SupportSQLiteStatement;
+import java.lang.Class;
+import java.lang.Exception;
+import java.lang.Override;
+import java.lang.String;
+import java.lang.SuppressWarnings;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.Callable;
+
+@SuppressWarnings({"unchecked", "deprecation"})
+public final class UserDao_Impl implements UserDao {
+  private final RoomDatabase __db;
+
+  private final EntityInsertionAdapter<User> __insertionAdapterOfUser;
+
+  private final EntityDeletionOrUpdateAdapter<User> __deletionAdapterOfUser;
+
+  private final EntityDeletionOrUpdateAdapter<User> __updateAdapterOfUser;
+
+  private final SharedSQLiteStatement __preparedStmtOfDeleteAllUsers;
+
+  public UserDao_Impl(RoomDatabase __db) {
+    this.__db = __db;
+    this.__insertionAdapterOfUser = new EntityInsertionAdapter<User>(__db) {
+      @Override
+      public String createQuery() {
+        return "INSERT OR ABORT INTO `users` (`id`,`user`,`favPlace`,`friend`) VALUES (?,?,?,?)";
+      }
+
+      @Override
+      public void bind(SupportSQLiteStatement stmt, User value) {
+        if (value.getId() == null) {
+          stmt.bindNull(1);
+        } else {
+          stmt.bindString(1, value.getId());
+        }
+        if (value.getName() == null) {
+          stmt.bindNull(2);
+        } else {
+          stmt.bindString(2, value.getName());
+        }
+        if (value.getFavPlace() == null) {
+          stmt.bindNull(3);
+        } else {
+          stmt.bindString(3, value.getFavPlace());
+        }
+        if (value.getFriend() == null) {
+          stmt.bindNull(4);
+        } else {
+          stmt.bindString(4, value.getFriend());
+        }
+      }
+    };
+    this.__deletionAdapterOfUser = new EntityDeletionOrUpdateAdapter<User>(__db) {
+      @Override
+      public String createQuery() {
+        return "DELETE FROM `users` WHERE `id` = ?";
+      }
+
+      @Override
+      public void bind(SupportSQLiteStatement stmt, User value) {
+        if (value.getId() == null) {
+          stmt.bindNull(1);
+        } else {
+          stmt.bindString(1, value.getId());
+        }
+      }
+    };
+    this.__updateAdapterOfUser = new EntityDeletionOrUpdateAdapter<User>(__db) {
+      @Override
+      public String createQuery() {
+        return "UPDATE OR ABORT `users` SET `id` = ?,`user` = ?,`favPlace` = ?,`friend` = ? WHERE `id` = ?";
+      }
+
+      @Override
+      public void bind(SupportSQLiteStatement stmt, User value) {
+        if (value.getId() == null) {
+          stmt.bindNull(1);
+        } else {
+          stmt.bindString(1, value.getId());
+        }
+        if (value.getName() == null) {
+          stmt.bindNull(2);
+        } else {
+          stmt.bindString(2, value.getName());
+        }
+        if (value.getFavPlace() == null) {
+          stmt.bindNull(3);
+        } else {
+          stmt.bindString(3, value.getFavPlace());
+        }
+        if (value.getFriend() == null) {
+          stmt.bindNull(4);
+        } else {
+          stmt.bindString(4, value.getFriend());
+        }
+        if (value.getId() == null) {
+          stmt.bindNull(5);
+        } else {
+          stmt.bindString(5, value.getId());
+        }
+      }
+    };
+    this.__preparedStmtOfDeleteAllUsers = new SharedSQLiteStatement(__db) {
+      @Override
+      public String createQuery() {
+        final String _query = "DELETE FROM users";
+        return _query;
+      }
+    };
+  }
+
+  @Override
+  public void insert(final User user) {
+    __db.assertNotSuspendingTransaction();
+    __db.beginTransaction();
+    try {
+      __insertionAdapterOfUser.insert(user);
+      __db.setTransactionSuccessful();
+    } finally {
+      __db.endTransaction();
+    }
+  }
+
+  @Override
+  public void delete(final User user) {
+    __db.assertNotSuspendingTransaction();
+    __db.beginTransaction();
+    try {
+      __deletionAdapterOfUser.handle(user);
+      __db.setTransactionSuccessful();
+    } finally {
+      __db.endTransaction();
+    }
+  }
+
+  @Override
+  public void update(final User user) {
+    __db.assertNotSuspendingTransaction();
+    __db.beginTransaction();
+    try {
+      __updateAdapterOfUser.handle(user);
+      __db.setTransactionSuccessful();
+    } finally {
+      __db.endTransaction();
+    }
+  }
+
+  @Override
+  public void deleteAllUsers() {
+    __db.assertNotSuspendingTransaction();
+    final SupportSQLiteStatement _stmt = __preparedStmtOfDeleteAllUsers.acquire();
+    __db.beginTransaction();
+    try {
+      _stmt.executeUpdateDelete();
+      __db.setTransactionSuccessful();
+    } finally {
+      __db.endTransaction();
+      __preparedStmtOfDeleteAllUsers.release(_stmt);
+    }
+  }
+
+  @Override
+  public LiveData<List<User>> getAllUsers() {
+    final String _sql = "SELECT * FROM users";
+    final RoomSQLiteQuery _statement = RoomSQLiteQuery.acquire(_sql, 0);
+    return __db.getInvalidationTracker().createLiveData(new String[]{"users"}, false, new Callable<List<User>>() {
+      @Override
+      public List<User> call() throws Exception {
+        final Cursor _cursor = DBUtil.query(__db, _statement, false, null);
+        try {
+          final int _cursorIndexOfId = CursorUtil.getColumnIndexOrThrow(_cursor, "id");
+          final int _cursorIndexOfName = CursorUtil.getColumnIndexOrThrow(_cursor, "user");
+          final int _cursorIndexOfFavPlace = CursorUtil.getColumnIndexOrThrow(_cursor, "favPlace");
+          final int _cursorIndexOfFriend = CursorUtil.getColumnIndexOrThrow(_cursor, "friend");
+          final List<User> _result = new ArrayList<User>(_cursor.getCount());
+          while(_cursor.moveToNext()) {
+            final User _item;
+            final String _tmpId;
+            if (_cursor.isNull(_cursorIndexOfId)) {
+              _tmpId = null;
+            } else {
+              _tmpId = _cursor.getString(_cursorIndexOfId);
+            }
+            final String _tmpName;
+            if (_cursor.isNull(_cursorIndexOfName)) {
+              _tmpName = null;
+            } else {
+              _tmpName = _cursor.getString(_cursorIndexOfName);
+            }
+            final String _tmpFavPlace;
+            if (_cursor.isNull(_cursorIndexOfFavPlace)) {
+              _tmpFavPlace = null;
+            } else {
+              _tmpFavPlace = _cursor.getString(_cursorIndexOfFavPlace);
+            }
+            final String _tmpFriend;
+            if (_cursor.isNull(_cursorIndexOfFriend)) {
+              _tmpFriend = null;
+            } else {
+              _tmpFriend = _cursor.getString(_cursorIndexOfFriend);
+            }
+            _item = new User(_tmpId,_tmpName,_tmpFavPlace,_tmpFriend);
+            _result.add(_item);
+          }
+          return _result;
+        } finally {
+          _cursor.close();
+        }
+      }
+
+      @Override
+      protected void finalize() {
+        _statement.release();
+      }
+    });
+  }
+
+  @Override
+  public LiveData<User> getUser(final String userId) {
+    final String _sql = "SELECT * FROM users WHERE id=?";
+    final RoomSQLiteQuery _statement = RoomSQLiteQuery.acquire(_sql, 1);
+    int _argIndex = 1;
+    if (userId == null) {
+      _statement.bindNull(_argIndex);
+    } else {
+      _statement.bindString(_argIndex, userId);
+    }
+    return __db.getInvalidationTracker().createLiveData(new String[]{"users"}, false, new Callable<User>() {
+      @Override
+      public User call() throws Exception {
+        final Cursor _cursor = DBUtil.query(__db, _statement, false, null);
+        try {
+          final int _cursorIndexOfId = CursorUtil.getColumnIndexOrThrow(_cursor, "id");
+          final int _cursorIndexOfName = CursorUtil.getColumnIndexOrThrow(_cursor, "user");
+          final int _cursorIndexOfFavPlace = CursorUtil.getColumnIndexOrThrow(_cursor, "favPlace");
+          final int _cursorIndexOfFriend = CursorUtil.getColumnIndexOrThrow(_cursor, "friend");
+          final User _result;
+          if(_cursor.moveToFirst()) {
+            final String _tmpId;
+            if (_cursor.isNull(_cursorIndexOfId)) {
+              _tmpId = null;
+            } else {
+              _tmpId = _cursor.getString(_cursorIndexOfId);
+            }
+            final String _tmpName;
+            if (_cursor.isNull(_cursorIndexOfName)) {
+              _tmpName = null;
+            } else {
+              _tmpName = _cursor.getString(_cursorIndexOfName);
+            }
+            final String _tmpFavPlace;
+            if (_cursor.isNull(_cursorIndexOfFavPlace)) {
+              _tmpFavPlace = null;
+            } else {
+              _tmpFavPlace = _cursor.getString(_cursorIndexOfFavPlace);
+            }
+            final String _tmpFriend;
+            if (_cursor.isNull(_cursorIndexOfFriend)) {
+              _tmpFriend = null;
+            } else {
+              _tmpFriend = _cursor.getString(_cursorIndexOfFriend);
+            }
+            _result = new User(_tmpId,_tmpName,_tmpFavPlace,_tmpFriend);
+          } else {
+            _result = null;
+          }
+          return _result;
+        } finally {
+          _cursor.close();
+        }
+      }
+
+      @Override
+      protected void finalize() {
+        _statement.release();
+      }
+    });
+  }
+
+  public static List<Class<?>> getRequiredConverters() {
+    return Collections.emptyList();
+  }
+}
